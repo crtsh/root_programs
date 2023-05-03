c041cba468dd11d96e0981b47d4b4817cddf9b48
Rob Stradling <rob@sectigo.com>
2023-05-03 11:01:21 +0100

    Update Java cacerts parser to support new file format (password-less PKCS#12).

--- java_cacerts/go.mod
+++ java_cacerts/go.mod
@@ -1,5 +1,10 @@
 module github.com/crtsh/root_programs/java_cacerts
 
-go 1.14
+go 1.19
 
-require github.com/square/certigo v1.12.1
+require (
+	github.com/square/certigo v1.16.0
+	software.sslmate.com/src/go-pkcs12 v0.2.0
+)
+
+require golang.org/x/crypto v0.8.0 // indirect
--- java_cacerts/java_cacerts.go
+++ java_cacerts/java_cacerts.go
@@ -5,16 +5,47 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"fmt"
-	"github.com/square/certigo/jceks"
 	"os"
 	"strings"
+
+	"github.com/square/certigo/jceks"
+
+	p12 "software.sslmate.com/src/go-pkcs12"
 )
 
-func main() {
+func tryJKSKeyStore() []*x509.Certificate {
 	// Read and parse cacerts file.
-	key_store, err := jceks.LoadFromFile(os.Args[1], nil)
-	if err != nil {
-		panic(err)
+	if keyStore, err := jceks.LoadFromFile(os.Args[1], nil); err != nil {
+		return nil
+	} else {
+		listCerts := keyStore.ListCerts()
+		certs := make([]*x509.Certificate, len(listCerts))
+		for i, alias := range listCerts {
+			if certs[i], err = keyStore.GetCert(alias); err != nil {
+				panic(err)
+			}
+		}
+		return certs
+	}
+}
+
+func tryPasswordlessPKCS12() []*x509.Certificate {
+	if p12Data, err := os.ReadFile(os.Args[1]); err != nil {
+		return nil
+	} else if certs, err := p12.DecodeTrustStore(p12Data, ""); err != nil {
+		return nil
+	} else {
+		return certs
+	}
+}
+
+func main() {
+	// Read cacerts file.  Since https://github.com/openjdk/jdk/pull/5948 the format has been a password-less PKCS#12 file; before then, it was a JKS KeyStore object.
+	var certs []*x509.Certificate
+	if certs = tryPasswordlessPKCS12(); certs == nil {
+		if certs = tryJKSKeyStore(); certs == nil {
+			panic(fmt.Errorf("could not parse %s", os.Args[1]))
+		}
 	}
 
 	// Ensure that this root store update is atomic.
@@ -23,13 +54,7 @@ func main() {
 	fmt.Printf("DELETE FROM root_trust_purpose WHERE TRUST_CONTEXT_ID = 23;\n\n")
 
 	// Iterate through the certificates.
-	var cert *x509.Certificate
-	for _, alias := range key_store.ListCerts() {
-		cert, err = key_store.GetCert(alias)
-		if err != nil {
-			panic(err)
-		}
-
+	for _, cert := range certs {
 		sha256_cert := sha256.Sum256(cert.Raw)
 		fmt.Printf("-- %s\n", cert.Subject.String())
 		fmt.Printf("-- SHA-256: %s\n", strings.ToUpper(hex.EncodeToString(sha256_cert[:])))
